5b3b07c16bc44cf5437e369139338f97799461f0
Austin Gonderinger <austin.gonderinger1@outlook.com>
2025-12-12 14:38:41 -0600

    Delete unneeded files

--- convert.py
+++
@@ -1,5 +0,0 @@
-from PyQt6 import uic
-
-# Replace 'input.ui' and 'output.py' with your actual file names
-with open("bankAccGui.py", "w", encoding="utf-8") as fout:
-    uic.compileUi("bankAccGui.ui", fout)
